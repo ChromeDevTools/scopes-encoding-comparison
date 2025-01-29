86f1fa033022693e92ef4ad01e41efc9a1c8c907
Simon Zünd <szuend@chromium.org>
2025-01-29 10:45:46 +0100

    Cleanup sizes stat printing

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@
 // found in the LICENSE file.
 
 import { parseArgs } from "jsr:@std/cli/parse-args";
-import { gzip } from "jsr:@deno-library/compress";
-import { compress } from "https://deno.land/x/brotli/mod.ts";
 
 import { CODEC as BaseCodec } from "./base/codec.ts";
 import { CODEC as BaseNoSemicolonCodec } from "./base_no_semicolon/codec.ts";
@@ -14,18 +12,7 @@ import { CODEC as StripNamesCodec } from "./strip_names/strip_names.ts";
 import { CODEC as StripScopesCodec } from "./strip_scopes/strip_scopes.ts";
 import { Codec, ScopeInfo, SourceMapJson } from "./types.ts";
 import { assertEquals } from "@std/assert";
-
-const formatter = new Intl.NumberFormat("en-US");
-const format = formatter.format.bind(formatter);
-
-const deltaPercentFormatter = new Intl.NumberFormat("en-US", {
-  maximumFractionDigits: 2,
-  signDisplay: "always",
-  style: "percent",
-});
-const formatDeltaPercent = deltaPercentFormatter.format.bind(
-  deltaPercentFormatter,
-);
+import { SizesStats } from "./stats.ts";
 
 if (import.meta.main) {
   const flags = parseArgs(Deno.args, {
@@ -81,9 +68,8 @@ if (import.meta.main) {
       : flags["sizes-reference"] === "no-names"
       ? StripNamesCodec
       : BaseCodec;
-  dumpCodecsInfo([referenceCodec, ...codecs]);
 
-  const result: unknown[] = [];
+  const stats = new SizesStats(referenceCodec.name, filterSourceMapProps);
 
   for (const file of flags._) {
     const content = Deno.readTextFileSync(file.toString());
@@ -94,33 +80,17 @@ if (import.meta.main) {
     // Use a stripped source map as the base to add scopes info.
     // This allows codecs to add names in a sane order for potential better encoding.
     const strippedMap = StripScopesCodec.encode(scopesInfo, proposalMap);
-
     const referenceMap = referenceCodec.encode(scopesInfo, strippedMap);
-    const baseSizes = calculateMapSizes(referenceMap, undefined, filterSourceMapProps);
 
-    const codecSizes = codecs.map((codec) => {
+    for (const codec of codecs) {
       const newMap = codec.encode(scopesInfo, strippedMap);
       if (flags.verify) verifyCodec(codec, newMap, referenceInfo);
-      const sizes = calculateMapSizes(newMap, baseSizes, filterSourceMapProps);
-      return { Codec: codec.name, ...formatMapSizes(sizes) };
-    });
-
-    result.push({ File: file });
-    result.push({ Codec: referenceCodec.name, ...formatMapSizes(baseSizes) });
-    result.push(...codecSizes);
-    result.push({});
+      stats.addMap(newMap, referenceMap, file.toString(), codec.name);
+    }
   }
 
-  console.table(result, [
-    "File",
-    "Codec",
-    "Uncompressed size",
-    "Δ raw",
-    "Compressed size (gzip)",
-    "Δ gzip",
-    "Compressed size (brotli)",
-    "Δ brotli",
-  ]);
+  dumpCodecsInfo([referenceCodec, ...codecs]);
+  stats.logTable();
 }
 
 function dumpCodecsInfo(codecs: Codec[]) {
@@ -152,59 +122,4 @@ function verifyCodec(
   for (let i = 0; i < originalInfo.ranges.length; ++i) {
     assertEquals(decodedScopes.ranges[i], originalInfo.ranges[i]);
   }
-}
-
-interface MapSizes {
-  raw: number;
-  gzip: number;
-  brotli: number;
-  deltaRaw?: number;
-  deltaGzip?: number;
-  deltaBrotli?: number;
-}
-
-function calculateMapSizes(
-  map: SourceMapJson,
-  base: MapSizes | undefined,
-  props?: (keyof SourceMapJson)[],
-): MapSizes {
-  const encoder = new TextEncoder();
-  const mapToStringify = props
-    ? props.reduce((obj, key) => {
-        obj[key] = map[key];
-        return obj;
-      }, {} as any)
-    : map;
-  const data = encoder.encode(JSON.stringify(mapToStringify));
-  const gzipData = gzip(data);
-  const brotliData = compress(data);
-
-  const delta = (old: number, ne: number) => (ne - old) / old;
-  const deltaRaw = base ? delta(base.raw, data.length) : undefined;
-  const deltaGzip = base ? delta(base.gzip, gzipData.length) : undefined;
-  const deltaBrotli = base ? delta(base.brotli, brotliData.length) : undefined;
-
-  return {
-    raw: data.length,
-    gzip: gzipData.length,
-    brotli: brotliData.length,
-    deltaRaw,
-    deltaGzip,
-    deltaBrotli,
-  };
-}
-
-function formatMapSizes(map: MapSizes) {
-  const formatDelta = (delta?: number) => {
-    if (delta === undefined) return "";
-    return formatDeltaPercent(delta);
-  };
-  return {
-    ["Uncompressed size"]: format(map.raw),
-    ["Δ raw"]: formatDelta(map.deltaRaw),
-    ["Compressed size (gzip)"]: format(map.gzip),
-    ["Δ gzip"]: formatDelta(map.deltaGzip),
-    ["Compressed size (brotli)"]: format(map.brotli),
-    ["Δ brotli"]: formatDelta(map.deltaBrotli),
-  };
 }
---
+++ src/stats.ts
@@ -0,0 +1,132 @@
+// Copyright 2025 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import { gzip } from "jsr:@deno-library/compress";
+import { compress } from "https://deno.land/x/brotli@0.1.7/mod.ts";
+
+import { SourceMapJson } from "./types.ts";
+
+export class SizesStats {
+  readonly #stats = new Map<string, Map<string, MapSizes>>();
+  readonly #referenceSizes = new Map<string, MapSizes>();
+  readonly #filterSourceMapProps?: (keyof SourceMapJson)[];
+  readonly #referenceCodecName: string;
+
+  constructor(referenceCodecName: string, filterSourceMapProps?: (keyof SourceMapJson)[]) {
+    this.#referenceCodecName = referenceCodecName;
+    this.#filterSourceMapProps = filterSourceMapProps;
+  }
+
+  addMap(map: SourceMapJson, referenceMap: SourceMapJson, file: string, codecName: string): void {
+    const referenceSizes = this.#getOrCalculateRefSizes(file, referenceMap);
+    const sizes = this.#calculateMapSizes(map, referenceSizes);
+    const perFileStats = this.#perFileStats(file);
+    perFileStats.set(codecName, sizes);
+  }
+
+  logTable(): void {
+    const result: unknown[] = [];
+
+    for (const [file, perFileStats] of this.#stats) {
+      result.push({ File: file });
+      result.push({ Codec: this.#referenceCodecName, ...formatMapSizes(this.#referenceSizes.get(file)!)})
+      for (const [codecName, sizes] of perFileStats) {
+        result.push( { Codec: codecName, ...formatMapSizes(sizes)});
+      }
+      result.push({});
+    }
+
+    console.table(result, [
+      "File",
+      "Codec",
+      "Uncompressed size",
+      "Δ raw",
+      "Compressed size (gzip)",
+      "Δ gzip",
+      "Compressed size (brotli)",
+      "Δ brotli",
+    ]);
+  }
+
+  #getOrCalculateRefSizes(file: string, referenceMap: SourceMapJson): MapSizes {
+    let sizes = this.#referenceSizes.get(file);
+    if (!sizes) {
+      sizes = this.#calculateMapSizes(referenceMap, undefined);
+      this.#referenceSizes.set(file, sizes);
+    }
+    return sizes;
+  }
+
+  #perFileStats(file: string): Map<string, MapSizes> {
+    let map = this.#stats.get(file);
+    if (!map) {
+      map = new Map<string, MapSizes>();
+      this.#stats.set(file, map);
+    }
+    return map;
+  }
+
+  #calculateMapSizes(map: SourceMapJson, referenceSizes: MapSizes|undefined): MapSizes {
+    const encoder = new TextEncoder();
+    const mapToStringify = this.#filterSourceMapProps
+      ? this.#filterSourceMapProps.reduce((obj, key) => {
+          obj[key] = map[key];
+          return obj;
+        }, {} as any)
+      : map;
+    const data = encoder.encode(JSON.stringify(mapToStringify));
+    const gzipData = gzip(data);
+    const brotliData = compress(data);
+  
+    const delta = (old: number, ne: number) => (ne - old) / old;
+    const deltaRaw = referenceSizes ? delta(referenceSizes.raw, data.length) : undefined;
+    const deltaGzip = referenceSizes ? delta(referenceSizes.gzip, gzipData.length) : undefined;
+    const deltaBrotli = referenceSizes ? delta(referenceSizes.brotli, brotliData.length) : undefined;
+  
+    return {
+      raw: data.length,
+      gzip: gzipData.length,
+      brotli: brotliData.length,
+      deltaRaw,
+      deltaGzip,
+      deltaBrotli,
+    };
+  }
+}
+
+interface MapSizes {
+  raw: number;
+  gzip: number;
+  brotli: number;
+  deltaRaw?: number;
+  deltaGzip?: number;
+  deltaBrotli?: number;
+}
+
+const formatter = new Intl.NumberFormat("en-US");
+const format = formatter.format.bind(formatter);
+
+const deltaPercentFormatter = new Intl.NumberFormat("en-US", {
+  maximumFractionDigits: 2,
+  signDisplay: "always",
+  style: "percent",
+});
+const formatDeltaPercent = deltaPercentFormatter.format.bind(
+  deltaPercentFormatter,
+);
+
+function formatMapSizes(map: MapSizes) {
+  const formatDelta = (delta?: number) => {
+    if (delta === undefined) return "";
+    return formatDeltaPercent(delta);
+  };
+  return {
+    ["Uncompressed size"]: format(map.raw),
+    ["Δ raw"]: formatDelta(map.deltaRaw),
+    ["Compressed size (gzip)"]: format(map.gzip),
+    ["Δ gzip"]: formatDelta(map.deltaGzip),
+    ["Compressed size (brotli)"]: format(map.brotli),
+    ["Δ brotli"]: formatDelta(map.deltaBrotli),
+  };
+}
